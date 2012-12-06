838b2066d0bc251b3f44e709bef2900a7119e664
fago <nuppla@zites.net>
2012-12-07 00:28:15 +0100

    Added niceEdit editor and restructured annotate javascript.

--- profiles/iksce/modules/iksce_js/iksce.js
+++ profiles/iksce/modules/iksce_js/iksce.js
@@ -1,80 +1,88 @@
 (function ($) {
 
-  root = this;
-  Backbone = root.Backbone;
-  _ = root._;
-  VIE = root.VIE;
+  // Integrate with niceEdit editor.
+  var selector = '.nicEdit-main';
 
-  var selector = '.field-name-body';
-  logger = true ? console : {
+  var options = {
+    autoAccept : false,
+    debug : true
+  };
+
+  logger = options.debug ? console : {
     info: function() {},
     warn: function() {},
     error: function() {},
     log: function() {}
   };
 
+  var vie = new VIE();
+  vie.use(new vie.StanbolService(Drupal.settings.iksce.stanbol));
+
+  /**
+   * annotate.js integration with Drupal and nicEdit
+   */
+  Drupal.iksce_annotate = {
+
+    instantiate : function () {
+      $(selector).annotate({
+        vie: vie,
+        // typeFilter: ["http://dbpedia.org/ontology/Place", "http://dbpedia.org/ontology/Organisation", "http://dbpedia.org/ontology/Person"],
+        debug: options.debug,
+        autoAnalyze: false,
+        showTooltip: false,
+        decline: function(event, ui){
+          logger.info('decline event', event, ui);
+        },
+        select: function(event, ui){
+          logger.info('select event', event, ui);
+        },
+        remove: function(event, ui){
+          logger.info('remove event', event, ui);
+        },
+        success: function(event, ui){
+          logger.info('success event', event, ui);
+        },
+        error: function(event, ui){
+          logger.info('error event', event, ui);
+        }
+      });
+      logger.log('annotated');
+    },
+
+    enable : function() {
+      $(selector)
+        .each(function() {
+          $(this)
+            .annotate('enable', function(success){
+              if (success && options.autoAccept) {
+                Drupal.iksce_annotate.acceptAll();
+              }
+              else if (!success) {
+                logger.warn('No success.');
+              }
+            });
+        });
+    },
+
+    acceptAll : function() {
+      $(selector)
+        .each(function() {
+          $(this)
+            .annotate('acceptAll', function(report) {
+              logger.log('AcceptAll finished with the report:', report);
+            });
+        })
+    }
+  }
+
+
   Drupal.behaviors.iksce_annotate = {
+
     attach: function (context, settings) {
 
-      var vie = new VIE();
-      vie.use(new vie.StanbolService(settings.iksce.stanbol));
-
-      // Instantiate annotate.js.
-      function instantiate() {
-        $(selector).annotate({
-          vie: vie,
-          // typeFilter: ["http://dbpedia.org/ontology/Place", "http://dbpedia.org/ontology/Organisation", "http://dbpedia.org/ontology/Person"],
-          debug: true,
-          autoAnalyze: false,
-          showTooltip: false,
-          decline: function(event, ui){
-            console.info('decline event', event, ui);
-          },
-          select: function(event, ui){
-            console.info('select event', event, ui);
-          },
-          remove: function(event, ui){
-            console.info('remove event', event, ui);
-          },
-          success: function(event, ui){
-            console.info('success event', event, ui);
-          },
-          error: function(event, ui){
-            console.info('error event', event, ui);
-          }
-        });
-        logger.log('annotated');
-      }
-
-
-      function enable() {
-        $(selector)
-          .each(function() {
-            $(this)
-              .annotate('enable', function(success){
-                if(success){
-                  logger.log('success');
-                  acceptAll();
-                }
-                else {
-                  logger.log('no success');
-                }
-              });
-          });
-      }
-
-      function acceptAll() {
-        $(selector)
-          .each(function() {
-            $(this)
-              .annotate('acceptAll', function(report) {
-              logger.log('AcceptAll finished with the report:', report);
-          });
-        })
-      }
-
-      instantiate();
-      enable();
+      Drupal.iksce_annotate.instantiate();
+      Drupal.iksce_annotate.enable();
+      console.log('run');
     }
   };
 
