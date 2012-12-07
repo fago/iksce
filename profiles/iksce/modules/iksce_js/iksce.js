fa50432724604830ef1b50061e894e37058668d9
fago <nuppla@zites.net>
2012-12-07 01:14:34 +0100

    Added annotate button.

--- profiles/iksce/modules/iksce_js/iksce.js
+++ profiles/iksce/modules/iksce_js/iksce.js
@@ -49,15 +49,18 @@
       logger.log('annotated');
     },
 
-    enable : function() {
-      $(selector)
+    enable : function(button) {
+      $(button).find('.text').html('...');
+      $(button).closest('.form-textarea-wrapper').find(selector)
         .each(function() {
           $(this)
             .annotate('enable', function(success){
+              $(button).find('.text').html(Drupal.t('Annotate'));
               if (success && options.autoAccept) {
                 Drupal.iksce_annotate.acceptAll();
               }
               else if (!success) {
+                $(button).find('.text').html(Drupal.t('Error!'));
                 logger.warn('No success.');
               }
             });
@@ -80,8 +83,21 @@
 
     attach: function (context, settings) {
 
+      var image = Drupal.settings.basePath + 'profiles/iksce/libraries/nicedit/nicEditorIcons.gif';
+
+      // Add a custom button to niceEdit. Ugly html copied over and adapted.
+      $('.nicEdit-panel').append(
+        '<div style="float: left; margin-top: 2px;">' +
+          '<div style="height: 20px; opacity: 1;" class=" nicEdit-buttonContain      nicEdit-buttonEnabled">' +
+          '<div style="margin: 0px 5px; background-color: rgb(239, 239, 239); border: 1px solid rgb(239, 239, 239);" class=" nicEdit-button-undefined nicEdit-button-hover nicEdit-button-active">' +
+          '<div class="text" style="height: 18px; overflow: hidden; cursor: pointer;" class=" nicEdit-button" unselectable="on">' + Drupal.t('Annotate') + '</div>' +
+          '</div></div></div>').bind('click', function() {
+
+          Drupal.iksce_annotate.enable(this);
+        });
+
+
       Drupal.iksce_annotate.instantiate();
-      Drupal.iksce_annotate.enable();
       console.log('run');
     }
   };
